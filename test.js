ecec36e223d6e85f2ef15cd12f750176f3e9c45b
Francis <vishnujack4@gmail.com>
2021-11-17 11:09:00 +0530

    First version

---
+++ test.js
@@ -0,0 +1,27 @@
+const alert = require("./index");
+
+
+alert()
+
+alert({
+  type: `success`,
+    msg: `Everything finished!`,
+  name: `DONE`
+} );
+
+
+alert({
+  type: `warning`,
+  msg: `You didn't add something!`,
+} );
+
+
+alert({
+  type: `info`,
+  msg: `I am awesome!`,
+} );
+
+alert({
+  type: `error`,
+  msg: `Something went wrong`,
+});
